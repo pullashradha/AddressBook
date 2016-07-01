6c813e8f691266c3939c731876579a3792c89131
Shradha Pulla <pullashradha@gmail.com>
2016-07-01 08:14:34 -0600

    Create getters and setters for all variables, having some problems with formal parameters?

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,7 +9,19 @@ namespace AddressBook
   {
     public HomeModule()
     {
-      
+      Get ["/"] = _ => View ["create_contact.cshtml"];
+      Post ["/contact_created"] = _ => {
+        Contact newContact = new Contact
+        (
+          Request.Form ["first-name"],
+          Request.Form ["last-name"],
+          Request.Form ["phone-number"],
+          Request.Form ["email-address"],
+          Request.Form ["street-address"],
+          Request.Form ["state-address"]
+        );
+        return View ["contact_created.cshtml", newContact];
+      };
     }
   }
 }
--- Objects/AddressBook.cs
+++ Objects/AddressBook.cs
@@ -6,6 +6,90 @@ namespace AddressBook.Objects
 {
   public class Contact
   {
-    
+    private string _firstName;
+    private string _lastName;
+    private string _fullName;
+    private string _phoneNumber;
+    private string _emailAddress;
+    private string _streetAddress;
+    private string _stateAddress;
+    private string _fullAddress;
+    public Contact (string firstName, string lastName, string fullName, string phoneNumber, string emailAddress, string streetAddress, string stateAddress, string fullAddress)
+    {
+      _firstName = firstName;
+      _lastName = lastName;
+      _fullName = fullName;
+      _phoneNumber = phoneNumber;
+      _emailAddress = emailAddress;
+      _streetAddress = streetAddress;
+      _stateAddress = stateAddress;
+      _fullAddress = fullAddress;
+    }
+    public string GetFirstName()
+    {
+      return _firstName;
+    }
+    public void SetFirstName(string newFirstName)
+    {
+      _firstName = newFirstName;
+    }
+    public string GetLastName()
+    {
+      return _lastName;
+    }
+    public void SetLastName(string newLastName)
+    {
+      _lastName = newLastName;
+    }
+    public string GetFullName()
+    {
+      _fullName = (_firstName + " " + _lastName);
+      return _fullName;
+    }
+    public void SetFullName(string newFullName)
+    {
+      _fullName = newFullName;
+    }
+    public string GetPhoneNumber()
+    {
+      return _phoneNumber;
+    }
+    public void SetPhoneNumber(string newPhoneNumber)
+    {
+      _phoneNumber = newPhoneNumber;
+    }
+    public string GetEmailAddress()
+    {
+      return _emailAddress;
+    }
+    public void SetEmailAddress(string newEmailAddress)
+    {
+      _emailAddress = newEmailAddress;
+    }
+    public string GetStreetAddress()
+    {
+      return _streetAddress;
+    }
+    public void SetStreetAddress(string newStreetAddress)
+    {
+      _streetAddress = newStreetAddress;
+    }
+    public string GetStateAddress()
+    {
+      return _stateAddress;
+    }
+    public void SetStateAddress(string newStateAddress)
+    {
+      _stateAddress = newStateAddress;
+    }
+    public string GetFullAddress()
+    {
+      _fullAddress = (_streetAddress + ", " + _stateAddress);
+      return _fullAddress;
+    }
+    public void SetFullAddress(string newFullAddress)
+    {
+      _fullAddress = newFullAddress;
+    }
   }
 }
